7b5ec28dd4f90d3b9ff480ae7b067cb38bcca4ac
Prince Thind <princethind799@gmail.com>
2022-12-04 17:56:01 +0530

    add copyVersionContent function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fse = require("fs-extra");
+const fs = require("fs");
 const simpleGit = require("simple-git");
 const path = require("path");
 
@@ -12,6 +13,12 @@ main();
 async function main() {
   copyFolder();
   await switchToGhPages();
+  createFolder();
+  copyVersionContent();
+  await gitPush();
+  getURL();
+  removeFolder()
+
   // deleteFolder();
 }
 
@@ -19,7 +26,7 @@ async function main() {
 /*
 //make a temp copy--->
  //switch to gh-apges on copy--> 
- create  a folder based on input--> 
+ //create  a folder based on input--> 
  copy contents into this folder--> 
  git push -f-->
  provide url to user
@@ -38,4 +45,37 @@ async function switchToGhPages() {
   const git = simpleGit();
   await git.cwd(destDir);
   await git.checkout("gh-pages");
+  await git.clean("fdx")
+}
+
+function createFolder() {
+  const versionName = process.argv[2];
+  fs.mkdirSync(path.join(destDir, versionName));
+}
+
+function copyVersionContent() {
+  const versionName = process.argv[2];
+  const folderName = process.argv[3];
+
+  try {
+    fse.copySync(
+      path.join(srcDir, folderName),
+      path.join(destDir, versionName),
+      { overwrite: false }
+    );
+    console.log("success!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function gitPush() {}
+
+function getURL() {
+  
+}
+
+
+function removeFolder(){
+
 }
