f49aef48fd63a6c68a60c47b4ba84d86fd9d228e
Prince Thind <princethind799@gmail.com>
2022-12-04 17:24:23 +0530

    implement switch branch function

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 #!/usr/bin/env node
 
 const fse = require("fs-extra");
+const simpleGit = require("simple-git");
 const path = require("path");
 
 const srcDir = process.cwd();
 const destDir = path.join(__dirname, "temp");
 
-
 main();
 
-function main() {
+async function main() {
   copyFolder();
-    
+  await switchToGhPages();
+  // deleteFolder();
 }
 
 //todo
 /*
 //make a temp copy--->
- switch to gh-apges on copy--> 
+ //switch to gh-apges on copy--> 
  create  a folder based on input--> 
  copy contents into this folder--> 
  git push -f-->
  provide url to user
 */
 
-
 function copyFolder() {
-
-
   try {
     fse.copySync(srcDir, destDir, { overwrite: true });
     console.log("success!");
   } catch (err) {
     console.error(err);
   }
+}
+
+async function switchToGhPages() {
+  const git = simpleGit();
+  await git.cwd(destDir);
+  await git.checkout("gh-pages");
 }
