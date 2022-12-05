872cd25282b4f6d49f66c197137f336ad7dafd40
Prince Thind <princethind799@gmail.com>
2022-12-05 18:02:54 +0530

    fix some fatal bugs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const path = require("path");
+const os = require("os");
 
 const argv = require("minimist")(process.argv.slice(2));
 
@@ -9,7 +10,7 @@ const { switchToGhPages, initializeGit, gitPush, printUrl } = require("./lib/git
 const { fetchGhpagesToOriginalRepo, linkGhPagesToLatest } = require("./lib/misc");
 
 const srcDir = process.cwd(); //repo path where it the cli is used
-const destDir = path.join(__dirname, "temp"); // copy of the repo we want to work with (inside this repo)
+const destDir = path.join(os.tmpdir(), "github_pages_temp"); // copy of the repo we want to work with (inside temp folder)
 
 const versionName = argv.version??'latest';
 const publishDir = argv.dir;
